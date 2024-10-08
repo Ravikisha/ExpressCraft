40dae84046f1722a1cb5e2a2243acaa664fef08a
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-08 14:17:04 +0530

    Adding Sample Code

--- modules/Code.js
+++ modules/Code.js
@@ -2,23 +2,62 @@ import { execSync } from "child_process";
 import fs from "fs";
 
 export default class Code {
-    constructor(language, packageManager){
-        this.language = language;
-        this.packageManager = packageManager;
-    }
+  constructor(language, packageManager) {
+    this.language = language;
+    this.packageManager = packageManager;
+  }
 
-    /**
-     * Create a src folder and create a index file
-     */
-    createSrcFolder(){
-        fs.mkdirSync("src");
-        if (this.language === "typescript"){
-            fs.writeFileSync("src/index.ts", "");
-        }else if (this.language === "javascript"){
-            fs.writeFileSync("src/index.js", "");
-        }else{
-            console.log("❌ Language not supported");
-            return
-        }
+  typescriptExpressTemplate() {
+    const sampleCode = `
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+    console.log(\`Server running on port \${port}\`);
+});
+            `;
+    return sampleCode;
+  }
+
+  javascriptExpressTemplate() {
+    const sampleCode = `
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, JavaScript with Express!');
+});
+
+app.listen(port, () => {
+    console.log(\`Server running on port \${port}\`);
+});
+            `;
+    return sampleCode;
+  }
+
+  /**
+   * Create a src folder and create a index file
+   */
+  createSrcFolder() {
+    fs.mkdirSync("src");
+    if (this.language === "typescript") {
+      fs.writeFileSync("src/index.ts", this.typescriptExpressTemplate());
+    } else if (this.language === "javascript") {
+      fs.writeFileSync("src/index.js", this.javascriptExpressTemplate());
+    } else {
+      console.log("❌ Language not supported");
+      return;
     }
+  }
 }
